fb1f27fc9644f024aa01f3b2724cde69c98986d0
Simone <simone@duiker101.net>
2019-06-03 17:04:44 +0200

    loading and better handling of tabs

--- src/background.js
+++ src/background.js
@@ -1,41 +1,83 @@
-// TODO use regex for matching
-var toTestLinks = ["/career", "/jobs", "/work"];
-var toTestTitles = ["Jobs", "Career", "Work with"];
-var jobs = {};
+let toTestLinks = ["/career", "/jobs", "/work"];
+let toTestTitles = ["Jobs", "Career", "Work with"];
+let jobs = {};
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+let currentTab = 0;
+
+chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({url: jobs[tab.id].href});
 });
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    if(currentTab > 0)
+        updateIcon(currentTab);
+});
+
+chrome.tabs.onActiveChanged.addListener(function (tabId, info) {
+    currentTab = tabId;
+    updateIcon(tabId);
+});
+
+function updateIcon(tabId) {
+    let found = jobs[tabId];
+    if (jobs[tabId]) {
+        if (found === 1) {
+            chrome.browserAction.setIcon({path: 'blue.png', tabId: tabId});
+        } else if (found === 2) {
+            chrome.browserAction.setIcon({path: 'green.png', tabId: tabId});
+        }
+    } else if (found === 0) {
+        chrome.browserAction.setIcon({path: 'red.png', tabId: tabId});
+        chrome.browserAction.disable(tabId);
+    }
+}
 
 // Receive message that the page has been loaded
 chrome.runtime.onMessage.addListener(
     function (data, sender, response) {
-        var tabId = sender.tab.id;
-        var found = null;
+        let tabId = sender.tab.id;
+
+        if (data.action === 'loading') {
+            chrome.browserAction.setIcon({path: 'yellow.png', tabId: tabId});
+            return;
+        }
+
+        let links = data.data.links;
+        let allText = data.data.text;
+        let found = null;
 
         // test the links and titles
-        for (var l of data) {
-            for (var t of toTestLinks) {
+        for (let l of links) {
+            for (let t of toTestLinks) {
                 if (l.href.indexOf(t) >= 0) {
                     found = l;
                     break;
                 }
             }
-            for (var t of toTestTitles) {
+
+            for (let t of toTestTitles) {
                 if (l.title.indexOf(t) >= 0) {
                     found = l;
                     break;
                 }
             }
+
             if (found !== null)
                 break
         }
 
         // a link has been found, update the browser action
         if (found !== null) {
-            jobs[tabId] = found;
-            chrome.browserAction.setIcon({path: 'green.png', tabId: tabId});
+            if (allText.toLocaleLowerCase().indexOf('remote') >= 0) {
+                jobs[tabId] = 1;
+                chrome.browserAction.setIcon({path: 'blue.png', tabId: tabId});
+            } else {
+                jobs[tabId] = 2;
+                chrome.browserAction.setIcon({path: 'green.png', tabId: tabId});
+            }
         } else {
+            jobs[tabId] = 0;
+            chrome.browserAction.setIcon({path: 'red.png', tabId: tabId});
             chrome.browserAction.disable(tabId);
         }
     }
--- src/content.js
+++ src/content.js
@@ -1,13 +1,27 @@
-window.onload = function(){
-    var result = [], links = document.links;
+// chrome.runtime.sendMessage({action: 'loading'});
+
+window.addEventListener('focus', function() {
+    sendMsg('focus');
+});
+
+window.onload = function () {
+    sendMsg('load');
+};
+
+function sendMsg(source) {
+    let result = [], links = document.links;
 
     // get all the links and put them in the response
-    for (var l of links) {
-    	var title = l.text.replace( /^\s+|\s+$/g, '' );
-    	var link = l.href;
-        result.push({href:link,title:title});
+    for (let l of links) {
+        let title = l.text.replace(/^\s+|\s+$/g, '');
+        let link = l.href;
+        result.push({href: link, title: title});
     }
 
     // return it to the background page
-    chrome.runtime.sendMessage(result);
-};
+    chrome.runtime.sendMessage({
+        action: 'loaded',
+        from: source,
+        data: {text: document.documentElement.textContent, links: result}
+    });
+}
